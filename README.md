89a67bdf07cf69dc1e89a7963f51c1e756823f14
hunter-heidenreich <hunter.scott.heidenreich@gmail.com>
2018-07-19 00:33:50 +0200

    [FEAT] Add saving of plots as an option

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Map-My-Writing
 Doing NLP analysis on text file collections (to visualize, to map, to understand)
+
+## Outline
+- Word Frequency Analysis
+- Basic Vocab Statistics 
+
+## Word Frequency Analysis
+![](visualizations/word_freq_bar_raw.png)
+![](visualizations/word_freq_bar_sorted.png)
+
+## Basic Vocab Statistics 
+![](visualizations/basic_stats_table.png)
--- graphs.py
+++ graphs.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt
 
 
-def plot_bar_graph(x, y, x_label='X', y_label='Y', title='Title'):
+def plot_bar_graph(x, y, x_label='X', y_label='Y', title='Title', export=False, export_name='default.png'):
     """
     Plots a bar graph
 
@@ -10,6 +10,8 @@ def plot_bar_graph(x, y, x_label='X', y_label='Y', title='Title'):
     :param x_label: The label of the x-axis -- str
     :param y_label: The label of the y-axis -- str
     :param title: The title of the graph -- str
+    :param export: Whether or not to export the graph to a file -- bool
+    :param export_name: The name of the file to save the graph to -- str
     :return: Nothing. -- It plots a bar chart
     """
     plt.bar(x, y)
@@ -18,20 +20,32 @@ def plot_bar_graph(x, y, x_label='X', y_label='Y', title='Title'):
     plt.title(title)
     plt.xlim(0, len(x))
     plt.ylim(0, max(y))
+
+    if export:
+        plt.savefig(export_name)
+
     plt.show()
 
 
-def plot_table(cell_data, row_labels=None, col_labels=None):
+def plot_table(cell_data, row_labels=None, col_labels=None, export=False, export_name='default.png'):
     """
     Creates a table with matplotlib
 
     :param cell_data: Two dimensional data representing the values in the table -- List[List[any]]
     :param row_labels: The labels of the rows -- List[any]
     :param col_labels: The labels of the columns -- List[any]
+    :param export: Whether or not to export the graph to a file -- bool
+    :param export_name: The name of the file to save the graph to -- str
     :return: Nothing. -- It creates and plots a table
     """
-    ax = plt.subplot(111, frame_on=False)
-    ax.xaxis.set_visible(False)
-    ax.yaxis.set_visible(False)
+    _ = plt.figure(figsize=(6, 1))
 
-    ax.table(cellText=cell_data, rowLabels=row_labels, colLabels=col_labels)
+    _ = plt.table(cellText=cell_data, rowLabels=row_labels, colLabels=col_labels, loc='center')
+
+    plt.axis('off')
+    plt.grid(False)
+
+    if export:
+        plt.savefig(export_name, bbox_inches='tight')
+
+    plt.show()
